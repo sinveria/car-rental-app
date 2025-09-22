481f41e1ccb2a57fa7d3ab27ec6e13011236ff0e
sinveria <perevalovav0108@gmail.com>
2025-09-22 14:48:14 +0300

    add getting started screen

---
+++ app/src/main/java/ru/sinveria/rentcar/ui/screens/GettingStarted.kt
@@ -0,0 +1,102 @@
+package ru.sinveria.rentcar.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import ru.sinveria.rentcar.R
+
+@Preview
+@Composable
+fun GettingStarted() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "DriveNext",
+            fontSize = 24.sp,
+            color = Color(0xFF2A1246),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 112.dp)
+        )
+
+        Text(
+            text = "Поможем найти твою следующую поездку",
+            fontSize = 16.sp,
+            color = Color(0xB2000000),
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.splash_image),
+            contentDescription = "Splash image",
+            modifier = Modifier
+                .padding(top = 80.dp)
+                .size(343.dp)
+        )
+
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .height(56.dp),
+
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF2A1246),
+                contentColor = Color.White
+            )
+        ) {
+            Text(
+                text = "Войти",
+                fontSize = 16.sp
+            )
+        }
+        Button(
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+                .height(56.dp)
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFFD0D5DD),
+                    shape = RoundedCornerShape(12.dp)
+                ),
+
+            shape = RoundedCornerShape(12.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFFFFFF),
+                contentColor = Color.Black
+            )
+        ) {
+            Text(
+                text = "Зарегистрироваться",
+                fontSize = 16.sp
+            )
+        }
+    }
+}
